88fb7efa3d893311d7654f96383933a0be3c622d
kingalban <kingalban@gmail.com>
2020-12-29 19:49:43 +0200

    part 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Blog = ({ blog }) => {
+    const [showBlog, setShowBlog] = useState(true)
+
+    const toggleShow = () => {
+        setShowBlog(!showBlog)
+    }
+
     return ( 
-        <div>
-            {blog.title} {blog.author}
+        <div className="blogEntry">
+            {showBlog 
+            ? <div> 
+                    {blog.title} {blog.author}
+                <button onClick={toggleShow}>
+                    view
+                </button> 
+            </div>
+            : <div> 
+                {blog.title} 
+                <button onClick={toggleShow}>
+                    hide
+                </button> <br/>
+                <a href={blog.url}>{blog.url}</a> <br/>
+                {blog.likes}
+                <button onClick={() => console.log("liked!")}>
+                    like
+                </button>  <br/>
+                {blog.author}
+            </div>
+                
+            }
         </div>
     )
 }
